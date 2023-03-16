3de326867d81b4db00633b5900d6f039acf13033
Alexandre Amory <alexandre.amory@santannapisa.it>
2023-03-16 11:11:52 +0100

    a remaider to improve the cleaning process

--- clean.sh
+++ clean.sh
@@ -5,6 +5,7 @@ cd fred-server.app || exit; . ELINOS.sh
 make distclean
 cd ..
 cd zcu102_hwvirt_linux.app || exit; . ELINOS.sh
+# TODO: this rule is erasing important binary files in zcu102_hwvirt_linux.app/app.rootfs/. Please fix me !
 make distclean
 cd ..
 cd zcu102_hwvirt_pikeos.int || exit; . ELINOS.sh;
