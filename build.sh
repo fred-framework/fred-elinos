d81133a6ad0ea10c59991ed033868a470954cf29
Alexandre Amory <alexandre.amory@santannapisa.it>
2023-03-27 17:34:39 +0200

    fixing some hardcoded absolute path

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ cd ..
 cd zcu102_hwvirt_linux.app || exit; /opt/elinos-7.1/bin/elinos-share-project import; . ELINOS.sh
 make boot
 cd ..
-cd zcu102_hwvirt_pikeos.int || exit; /opt/pikeos-5.1/bin/pikeos-share-project import; . ELINOS.sh;
+cd zcu102_hwvirt_pikeos.int || exit; /opt/pikeos-5.1/bin/pikeos-share-project import;
 make all
 cd ..
