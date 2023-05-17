70363f03cc12b93917ba092aaa5e1667448bb4f8
Gabriele Ara <gabriele.ara@santannapisa.it>
2023-05-17 14:52:40 +0200

    Updated build and burn scripts

--- build.sh
+++ build.sh
@@ -1,12 +1,107 @@
-cd fred-lib.app || exit; ./configure -n; . ELINOS.sh
-make install
-cd ..
-cd fred-server.app || exit; ./configure -n; . ELINOS.sh
-make install
-cd ..
-cd zcu102_hwvirt_linux.app || exit; /opt/elinos-7.1/bin/elinos-share-project import; . ELINOS.sh
-make boot
-cd ..
-cd zcu102_hwvirt_pikeos.int || exit; /opt/pikeos-5.1/bin/pikeos-share-project import;
-make all
-cd ..
+#!/bin/bash
+
+function build_fred_library() {
+    cd fred-lib.app
+    ./configure -n
+    source ELINOS.sh
+    make install
+    cd ..
+}
+
+function build_fred_server() {
+    cd fred-server.app
+    ./configure -n
+    source ELINOS.sh
+    make install
+    cd ..
+}
+function build_zcu102_hwvirt_linux() {
+    cd zcu102_hwvirt_linux.app
+    /opt/elinos-7.1/bin/elinos-share-project import
+    source ELINOS.sh
+    make boot
+    cd ..
+}
+function build_zcu102_hwvirt_pikeos() {
+    cd zcu102_hwvirt_pikeos.int
+    /opt/pikeos-5.1/bin/pikeos-share-project import || true # Must accept that this task may fail for a simple warning
+    make all
+    cd ..
+}
+
+function run_step() {
+    echo " +-- Running Step '$1' ..."
+    if ! "$2" > "$OUTFILE" ; then
+        echo " +-- Error while running Step '$1', exiting..."
+        return 1
+    fi
+}
+
+function usage() {
+    cat <<EOF
+
+USAGE: ${BASH_SOURCE[0]} [options]
+
+OPTIONS:
+    -h, --help          Display this help message
+    -v, --verbose       Print output of all commands
+                        NOTE: by default this script hides
+                              stdout, but not stderr.
+
+EOF
+}
+
+function main() {
+    VERBOSE=0
+
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+        -h|--help)
+            usage
+            return 0
+            ;;
+        -v|--verbose)
+            VERBOSE=1
+            ;;
+        *)
+            echo "ERROR: Unrecognized option '$1'" >&2
+            usage
+            return 1
+        esac
+        shift
+    done
+
+    SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+    cd "$SCRIPT_DIR"
+
+    if [ "$VERBOSE" = 1 ]; then
+        OUTFILE=/dev/stdout
+    else
+        OUTFILE=/dev/null
+    fi
+
+    # Cleanup, just in case
+    # rm -f fred-lib.app/ELINOS.sh
+    # rm -f fred-server.app/ELINOS.sh
+    # rm -f zcu102_hwvirt_linux.app/ELINOS.sh
+    # rm -f zcu102_hwvirt_pikeos.int/ELINOS.sh
+
+    # Restart the server, just in case
+    echo " +-- Restarting the license server"
+    echo "     You might be prompted for your password..."
+    sudo systemctl stop lmxserv51
+    sudo rm -f /usr/lmx-5.1/lmx-serv.pid
+    sudo systemctl start lmxserv51
+
+    run_step "Fred Library" build_fred_library
+    run_step "Fred Server" build_fred_server
+    run_step "ZCU102 HwVirt Linux" build_zcu102_hwvirt_linux
+    run_step "ZCU102 HwVirt PikeOS" build_zcu102_hwvirt_pikeos
+
+    echo ' +-- Success!'
+}
+
+(
+    set -e
+    main "$@"
+)
--- burn.sh
+++ burn.sh
@@ -1,3 +1,133 @@
-cd zcu102_hwvirt_pikeos.int || exit; . ELINOS.sh;
-make install
-cd ..
+#!/bin/bash
+
+function usage() {
+    cat <<EOF
+
+USAGE: ${BASH_SOURCE[0]} [options] DESTINATION_UBOOT DESTINATION_ROOTFS
+
+OPTIONS:
+    -h, --help          Display this help message
+EOF
+}
+
+function main() {
+    if [ "$(id -u)" != 0 ]; then
+        echo "Please run this script as root or with sudo" >&2
+        return 1
+    fi
+
+    POSITIONAL_ARGS=0
+    NEXT_POSITIONA_ARG=(DESTINATION_UBOOT DESTINATION_ROOTFS)
+
+    DESTINATION_UBOOT=
+    DESTINATION_ROOTFS=
+
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+        -h|--help)
+            usage
+            return 0
+            ;;
+        -*)
+            echo "ERROR: Unrecognized option '$1'" >&2
+            usage
+            return 1
+            ;;
+        *)
+            if [ "$POSITIONAL_ARGS" -ge 2 ] ; then
+                echo "ERROR: too many positional arguments!" >&2
+                usage
+                return 1
+            fi
+
+            declare "${NEXT_POSITIONA_ARG[$POSITIONAL_ARGS]}"="$1"
+            POSITIONAL_ARGS=$((POSITIONAL_ARGS+1))
+        esac
+        shift
+    done
+
+    if [ -z "$DESTINATION_UBOOT" ] || [ -z "$DESTINATION_ROOTFS" ]; then
+        echo "ERROR: required argument missing" >&2
+        usage
+        return 1
+    fi
+
+    if ! [ -d "$DESTINATION_UBOOT" ] || [ "$DESTINATION_UBOOT" = / ]; then
+        echo "Invalid argument DESTINATION_UBOOT: '$DESTINATION_UBOOT'" >&2
+        usage
+        return 1
+    fi
+
+    if ! [ -d "$DESTINATION_ROOTFS" ] || [ "$DESTINATION_ROOTFS" = / ]; then
+        echo "Invalid argument DESTINATION_ROOTFS: '$DESTINATION_ROOTFS'" >&2
+        usage
+        return 1
+    fi
+
+    DESTINATION_UBOOT="$(realpath "$DESTINATION_UBOOT")"
+    DESTINATION_ROOTFS="$(realpath "$DESTINATION_ROOTFS")"
+
+    echo ""
+    echo "About to write to the following directories,"
+    echo "please type 'y' to confirm."
+    echo "Anything else will exit."
+    echo ""
+    echo "boot:     '$DESTINATION_UBOOT'"
+    echo "rootfs:   '$DESTINATION_ROOTFS'"
+    echo ""
+    read -r -p "Are you sure? [y/N] " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            # Continue
+            ;;
+        *)
+            echo "I'll take that as a no. Exiting."
+            return 1
+            ;;
+    esac
+
+    SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+    cd "$SCRIPT_DIR"
+
+    MAKEFILE="zcu102_hwvirt_pikeos.int/Makefile"
+    if ! [ -f "$MAKEFILE.bkp" ]; then
+        echo "" >&2
+        echo "WARN: Backing up file '$MAKEFILE'." >&2
+        echo "If this is the first time you run this script this is expected." >&2
+        echo "" >&2
+        cp "$MAKEFILE" "$MAKEFILE.bkp"
+    fi
+
+    echo " + Setting target directories as instructed..."
+
+    # Substitute destination directories
+    tmpfile=$(mktemp)
+    cp "$MAKEFILE.bkp" "$tmpfile"
+    sed -i 's#/media/ubuntu/ROOT1#'"\"$DESTINATION_ROOTFS\""'#g' "$tmpfile"
+    sed -i 's#/media/ubuntu/ROOT#'"\"$DESTINATION_UBOOT\""'#g' "$tmpfile"
+    sed -i 's/umount/#umount/g/' "$tmpfile"
+    # TODO: rename to image.ub
+    cp "$tmpfile" "$MAKEFILE"
+    rm -f "$tmpfile"
+
+    echo " + Cleaning target directories..."
+    rm -rf "${DESTINATION_UBOOT:?}"/*
+    rm -rf "${DESTINATION_ROOTFS:?}"/*
+
+    echo " + Burning on target directories..."
+    cd zcu102_hwvirt_pikeos.int
+    # source ELINOS.sh
+    source PikeOS.sh
+    make install
+    cd ..
+
+    mv "$DESTINATION_UBOOT"/hwvirt-linux-zynq-zcu102-hwvirt-uboot_dtb "$DESTINATION_UBOOT/image.ub"
+    umount "$DESTINATION_UBOOT"
+    umount "$DESTINATION_ROOTFS"
+    echo " + Success"
+}
+
+(
+    set -e
+    main "$@"
+)
